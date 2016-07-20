b77275935de19163033bd0a22c5232eb7c537ab0
vagrant <vagrant@precise32.(none)>
2016-07-20 20:19:41 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Music::Application.config.secret_key_base = '6cc5831a1e0076c0b033a41c7775f87d7dd727f155ca88c2d9f9458d927ae8780f5ff07e9c4adf0c3b469cebe09465bc0f309c8f429b5f627e521a94877ddb7a'
